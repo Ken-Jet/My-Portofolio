8f716c196fadabdacafc582c5619f526c3d42259
Ken-Jet <rachelmaniac535@gmail.com>
2025-11-05 19:56:38 +0700

    melakukan update code

--- Web/index.js
+++ Web/index.js
@@ -73,27 +73,253 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
     trigger : '.box-7',
+    end : 'center',
     onEnter : () => gsap.to('.badan' , {backgroundColor : "#c79ee6" , duration : 1 , ease : 'power1.out'}),
     onLeave : () => gsap.to('.badan' , {backgroundColor : 'white', duration : 1, ease : 'power1.out'}),
     onEnterBack : () => gsap.to ('.badan' , {backgroundColor : "#c79ee6" , duration : 1 , ease : 'power1.out'}),
     onLeaveBack : () => gsap.to('.badan' , {backgroundColor : '#a1e69e', duration : 1, ease : 'power1.out'}),
+    markers : false
 });
 
-var tl2 = gsap.timeline();
+var tl2 = gsap.timeline({
+    scrollTrigger : {
+        trigger : '.box-8',
+        ease : 'power1.InOut'
+    }
+});
 
 tl2.from('.gambar-1' ,{
-    scrollTrigger : '.gambar-1',
     duration : 1,
     opacity : 0,
     y: -100,
     ease : 'power4.out'
-}) .then(() => {
-    gsap.fromTo('.gambar-1' , {
-        y : 0
+}) 
+.fromTo('.gambar-1' , {
+        y : 0,
+        ease : 'none'
     }, 
     
     {
-        y : -150,
-        duration : 1
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
     })
+
+.from('.gambar-2' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 0.25) 
+.fromTo('.gambar-2' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-3' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 0.50) 
+.fromTo('.gambar-3' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-4' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 0.75) 
+.fromTo('.gambar-4' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+    
+.from('.gambar-5' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 1) 
+.fromTo('.gambar-5' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-6' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 1.25) 
+.fromTo('.gambar-6' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+
+var tl3 = gsap.timeline({
+    scrollTrigger : {
+        trigger : '.box-7',
+        ease : 'power1.InOut'
+    }
 });
+
+tl3.from('.gambar-7' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}) 
+.fromTo('.gambar-7' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-8' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 0.25) 
+.fromTo('.gambar-8' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-9' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 0.50) 
+.fromTo('.gambar-9' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-10' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 0.75) 
+.fromTo('.gambar-10' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+    
+.from('.gambar-11' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 1) 
+.fromTo('.gambar-11' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
+
+.from('.gambar-12' ,{
+    duration : 1,
+    opacity : 0,
+    y: -100,
+    ease : 'power4.out'
+}, 1.25) 
+.fromTo('.gambar-12' , {
+        y : 0,
+        ease : 'none'
+    }, 
+    
+    {
+        y : -50,
+        duration : 1.5,
+        repeat : -1,
+        yoyo : true,
+        ease : 'none'
+    })
